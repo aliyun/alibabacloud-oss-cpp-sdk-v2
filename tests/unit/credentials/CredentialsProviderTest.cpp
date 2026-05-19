fe8cf2822358b7542bc342937553927467d20573
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2026-05-19 15:42:02 +0800

    add CredentialsProviderFunc wrapper for custom credential sources

--- tests/unit/credentials/CredentialsProviderTest.cpp
+++ tests/unit/credentials/CredentialsProviderTest.cpp
@@ -22,6 +22,30 @@ TEST(CredentialsProviderTest, StaticCredentialsProviderWithToken) {
     EXPECT_EQ("token", cred.getSessionToken());
 }
 
+TEST(CredentialsProviderTest, CredentialsProviderFunc) {
+    auto provider = CredentialsProviderFunc([]() {
+        return Credentials("func-ak", "func-sk", "func-token");
+    });
+    EXPECT_EQ(CredentialsProvider::AuthType::DEFAULT, provider.getAuthType());
+    auto cred = provider.getCredentials();
+    EXPECT_EQ("func-ak", cred.getAccessKeyId());
+    EXPECT_EQ("func-sk", cred.getAccessKeySecret());
+    EXPECT_EQ("func-token", cred.getSessionToken());
+}
+
+TEST(CredentialsProviderTest, CredentialsProviderFunc_DynamicUpdate) {
+    int callCount = 0;
+    auto provider = CredentialsProviderFunc([&callCount]() {
+        callCount++;
+        return Credentials("ak-" + std::to_string(callCount), "sk", "");
+    });
+    auto cred1 = provider.getCredentials();
+    EXPECT_EQ("ak-1", cred1.getAccessKeyId());
+    auto cred2 = provider.getCredentials();
+    EXPECT_EQ("ak-2", cred2.getAccessKeyId());
+    EXPECT_EQ(2, callCount);
+}
+
 TEST(CredentialsProviderTest, AnonymousCredentialsProvider) {
     auto provider = AnonymousCredentialsProvider();
     EXPECT_EQ(CredentialsProvider::AuthType::ANONYMOUS, provider.getAuthType());
