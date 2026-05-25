69f38402620eb49d4cf83ae0152bf1c461be0ad9
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2026-05-25 09:46:00 +0800

    add e2e mock tests with cpp-httplib

--- tests/integration/sync/object/ObjectPresignTest.cpp
+++ tests/integration/sync/object/ObjectPresignTest.cpp
@@ -1,12 +1,9 @@
 #include <gtest/gtest.h>
 
-#include "alibabacloud/oss2/Config.h"
 #include "Config.h"
 #include "sync/ClientHelper.h"
 #include "HttpClients.h"
 #include "alibabacloud/oss2/OSSClient.h"
-
-#ifdef ALIBABACLOUD_OSS_HAS_CURL
 
 namespace alibabacloud {
 namespace oss2 {
@@ -216,5 +213,3 @@ TEST_F(ObjectPresignTest, PresignUploadPart) {
 } // namespace sync
 } // namespace oss2
 } // namespace alibabacloud
-
-#endif // ALIBABACLOUD_OSS_HAS_CURL
