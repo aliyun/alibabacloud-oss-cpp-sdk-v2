39037b4995ca15b4084a635702314d6ed92a53c1
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2026-05-25 09:46:00 +0800

    add e2e mock tests with cpp-httplib

--- tests/integration/HttpClients.h
+++ tests/integration/HttpClients.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include "alibabacloud/oss2/Config.h"
-
-#ifdef ALIBABACLOUD_OSS_HAS_CURL
-
-#include <curl/curl.h>
+#include <httplib.h>
 #include <string>
 #include <map>
-#include <stdexcept>
 
 namespace alibabacloud {
 namespace oss2 {
@@ -15,35 +10,95 @@ namespace test {
 
 class HttpClient {
   public:
-    HttpClient();
-    ~HttpClient();
-
     struct Response {
         long statusCode = 0;
         std::string body;
         std::map<std::string, std::string> headers;
     };
 
-    Response get(const std::string& url);
-    Response get(const std::string& url, const std::map<std::string, std::string>& headers);
-    Response put(const std::string& url, const std::string& data);
-    Response put(const std::string& url, const std::string& data, const std::map<std::string, std::string>& headers);
+    Response get(const std::string& url) {
+        return get(url, {});
+    }
 
-    void setConnectTimeout(long seconds);
-    void setReadWriteTimeout(long seconds);
+    Response get(const std::string& url, const std::map<std::string, std::string>& headers) {
+        auto [base, path] = splitUrl(url);
+        httplib::Client cli(base);
+        cli.set_connection_timeout(connectTimeout_);
+        cli.set_read_timeout(readWriteTimeout_);
+        cli.set_follow_location(true);
+        cli.set_url_encode(false);
+        httplib::Headers h(headers.begin(), headers.end());
+        auto res = cli.Get(path, h);
+        return toResponse(res);
+    }
+
+    Response put(const std::string& url, const std::string& data) {
+        return put(url, data, {});
+    }
+
+    Response put(const std::string& url, const std::string& data,
+                 const std::map<std::string, std::string>& headers) {
+        auto [base, path] = splitUrl(url);
+        httplib::Client cli(base);
+        cli.set_connection_timeout(connectTimeout_);
+        cli.set_read_timeout(readWriteTimeout_);
+        cli.set_follow_location(true);
+        cli.set_url_encode(false);
+        httplib::Headers h(headers.begin(), headers.end());
+
+        std::string contentType;
+        auto ct = h.find("Content-Type");
+        if (ct != h.end()) {
+            contentType = ct->second;
+            h.erase(ct);
+        }
+
+        auto res = cli.Put(path, h, data.size(),
+                [&data](size_t offset, size_t length, httplib::DataSink& sink) {
+                    sink.write(data.data() + offset, std::min(length, data.size() - offset));
+                    return true;
+                }, contentType);
+        return toResponse(res);
+    }
+
+    void setConnectTimeout(long seconds) {
+        connectTimeout_ = std::chrono::seconds(seconds);
+    }
+
+    void setReadWriteTimeout(long seconds) {
+        readWriteTimeout_ = std::chrono::seconds(seconds);
+    }
 
   private:
-    static size_t writeCallback(void* contents, size_t size, size_t nmemb, std::string* userp);
-    static size_t readCallback(void* buffer, size_t size, size_t nitems, std::string* userp);
-    static struct curl_slist* buildHeaderList(const std::map<std::string, std::string>& headers);
+    static std::pair<std::string, std::string> splitUrl(const std::string& url) {
+        // scheme://host[:port]/path[?query]
+        auto schemeEnd = url.find("://");
+        if (schemeEnd == std::string::npos) {
+            return {"http://" + url, "/"};
+        }
+        auto pathStart = url.find('/', schemeEnd + 3);
+        if (pathStart == std::string::npos) {
+            return {url, "/"};
+        }
+        return {url.substr(0, pathStart), url.substr(pathStart)};
+    }
 
-    CURL* curl_;
-    long connectTimeout_ = 10;
-    long readWriteTimeout_ = 30;
+    static Response toResponse(const httplib::Result& res) {
+        Response r;
+        if (res) {
+            r.statusCode = res->status;
+            r.body = res->body;
+            for (const auto& h : res->headers) {
+                r.headers[h.first] = h.second;
+            }
+        }
+        return r;
+    }
+
+    std::chrono::seconds connectTimeout_{10};
+    std::chrono::seconds readWriteTimeout_{30};
 };
 
 } // namespace test
 } // namespace oss2
 } // namespace alibabacloud
-
-#endif // ALIBABACLOUD_OSS_HAS_CURL
